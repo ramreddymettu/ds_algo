c763bc8e0d0b470eda240d951790a1082ef3e1f1
ramreddymettu <venkataramireddymettu@gmail.com>
2025-06-10 20:19:15 +0530

    interview question

--- interview_qstn/dijskartaj.py
+++ interview_qstn/dijskartaj.py
@@ -2,49 +2,43 @@
 import heapq
 
 def dijkstra_min_path(grid):
+    
     if not grid or not grid[0]:
-        return 0, []
-
+        return 0, [] 
+    
     m, n = len(grid), len(grid[0])
     start = (0, 0)
     end = (m - 1, n - 1)
-    print(f"Start - {start} \n End - {end}")
 
-    # Priority queue: (cost, i, j)
-    heap = [(grid[0][0], 0, 0)]
-    # Store minimum cost to reach each cell
-    cost = {(0, 0): grid[0][0]}
-    # To reconstruct path
+    heap = [(grid[0][0], 0, 0 )]
+    costd = {(0, 0): grid[0][0]}
     parent = {(0, 0): None}
 
-    directions = [(0, 1), (1, 0)]  # Right and Down
+    directions = [(0, 1), (1, 0)]
 
     while heap:
-        curr_cost, i, j = heapq.heappop(heap)
+        cost, i, j = heapq.heappop(heap)
 
         if (i, j) == end:
             break
 
-        for di, dj in directions:
-            ni, nj = i + di, j + dj
-            if 0 <= ni < m and 0 <= nj < n:
-                new_cost = curr_cost + grid[ni][nj]
-                if (ni, nj) not in cost or new_cost < cost[(ni, nj)]:
-                    cost[(ni, nj)] = new_cost
-                    parent[(ni, nj)] = (i, j)
-                    heapq.heappush(heap, (new_cost, ni, nj))
-                    print(f"Cost - {cost} \n Parent - {parent} \n Heaq - {heap}")
-                    print("*********************")
+        for ni, nj in directions:
+            di, dj = i + ni, j + nj
+            if 0 <= di < m and 0 <= dj < n:
+                new_cost = cost + grid[di][dj]
+                if (di, dj) not in costd or new_cost < costd[(ni, nj)]:
+                    costd[(di, dj)] = new_cost
+                    parent[(di, dj)] = (i, j)
+                    heapq.heappush(heap, (new_cost, di, dj))
 
-    # Reconstruct path from end to start
     path = []
     node = end
     while node:
         i, j = node
         path.append(grid[i][j])
         node = parent[node]
-
-    return cost[end], path[::-1]
+    
+    return costd[end], path[::-1]
 
 
 grid = [
